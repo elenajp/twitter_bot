a7c02831707d52cab41aa65d17bcde8c871b0920
elena perez <ejperezk@gmail.com>
2021-04-11 20:46:58 +0200

    added keys and key words to search tweets for. Also added 3 messages to for testing purposes

--- twitterbot.py
+++ twitterbot.py
@@ -3,3 +3,20 @@
 import os
 
 import tweepy
+
+from keys import keys
+
+API_KEY = keys['api_key']
+API_SECRET_KEY = keys['api_secret_key']
+BEARER_TOKEN = keys['bearer_token']
+
+auth = tweepy.OAuthHandler()
+auth.set_access_token()
+api = tweepy.API(auth)
+
+key_words = ['shark', 'sharks', 'shark week', 'sharkweek']
+msgs = [
+    'Being left-handed kills more people than sharks! 2, 500 people die each year after using right-handed items incorrectly. Sorry, lefties.',
+    'Armed toddlers kill more people than sharks! 21 people die each year from toddlers wielding a gun they found in an unlocked cabinet.',
+    'Elephants kill more people than sharks! Those cute, cuddly animals that everyone adores? Yeah, they kill 600 people a year. Tough love.'
+]
