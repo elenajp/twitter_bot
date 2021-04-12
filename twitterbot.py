93d5af5160a9a051612518a7e4e7adfdb22e857a
elena perez <ejperezk@gmail.com>
2021-04-12 16:27:14 +0200

    deleted unused bearer token variable, created a txt file to store tweet ids and the retrieve_id function to store the last id in the txt file

--- twitterbot.py
+++ twitterbot.py
@@ -6,15 +6,34 @@ import tweepy
 
 from keys import keys
 
-API_KEY = keys['api_key']
-API_SECRET_KEY = keys['api_secret_key']
-BEARER_TOKEN = keys['bearer_token']
-ACCESS_TOKEN = keys['access_token']
-ACCESS_TOKEN_SECRET = keys['access_token_secret']
+API_KEY = keys['API_KEY']
+API_SECRET_KEY = keys['API_SECRET_KEY']
+ACCESS_TOKEN = keys['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = keys['ACCESS_TOKEN_SECRET']
 
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+# api variable used to execute various methods and interact with twitter
 api = tweepy.API(auth)
+
+FILE = 'id.txt'
+
+# retrieve_id grabs id from text file used it, grab last id and save it in text file
+
+
+def retrieve_id(file):
+    f_read = open(file)  # opens the file
+    last_seen_id = int(f_read.read().script())  # grabs the id in the file
+    f_read.close()  # closes the file
+    return last_seen_id
+
+
+retrieve_id(FILE)
+
+
+mentions = api.mentions_timeline()
+for mention in mentions:
+    print(mention.id)
 
 key_words = ['shark', 'sharks', 'shark week', 'sharkweek']
 msgs = [
