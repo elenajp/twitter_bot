5b90b63f8e6c96887e4f2be3a8225e0c4bef216e
elena perez <ejperezk@gmail.com>
2021-04-13 17:07:28 +0200

    added for loop to loop through list of strings to check tweets for keywords

--- twitterbot.py
+++ twitterbot.py
@@ -8,9 +8,10 @@ API_SECRET_KEY = keys['API_SECRET_KEY']
 ACCESS_TOKEN = keys['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = keys['ACCESS_TOKEN_SECRET']
 
+key_words = ['#shark', '#sharks', '#sharkweek', 'sharks', 'shark', 'sharkweek']
+
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-# api variable used to execute various methods and interact with twitter
 api = tweepy.API(auth)
 
 FILE = 'id.txt'
@@ -30,21 +31,18 @@ def store_id(id, file):
     return
 
 
-# mentions = api.mentions_timeline()
-# for mention in mentions:
-#     print(mention.id)
-
 last_Seen_id = retrieve_id(FILE)
 mentions = api.mentions_timeline(last_Seen_id, tweet_mode='extended')
+
 for mention in reversed(mentions):
-    if 'shark' in mention.full_text:
-        last_Seen_id = mention.id
-        store_id(last_Seen_id, FILE)
-        api.update_status('@'+mention.user.screen_name + 'YAY', mention.id)
-        print('Replied to @' + mention.user.screen_name)
+    for i in key_words:
+        if i in mention.full_text:
+            last_Seen_id = mention.id
+            store_id(last_Seen_id, FILE)
+            api.update_status('@'+mention.user.screen_name + 'YAY', mention.id)
+            print('Replied to @' + mention.user.screen_name)
 
 
-key_words = ['#shark', '#sharks', '#sharkweek', 'sharks', 'shark']
 msgs = [
     'Being left-handed kills more people than sharks! 2, 500 people die each year after using right-handed items incorrectly. Sorry, lefties.',
     'Armed toddlers kill more people than sharks! 21 people die each year from toddlers wielding a gun they found in an unlocked cabinet.',
