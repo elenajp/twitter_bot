9d2cd95ae23e7d78a6fd6891713a5f3c5513b8b9
elena perez <ejperezk@gmail.com>
2023-02-02 10:09:39 +0100

    testing out in this file. I have the retweet_comment_and_like function working now

--- bot/newbot.py
+++ bot/newbot.py
@@ -1,35 +1,42 @@
-import keys
+#!/usr/bin/env python3
+import random
+
 import openai
 import tweepy
+from keys import keys
+from tweepy import Cursor
 
-# Twitter API credentials
-consumer_key = "CONSUMER_KEY"
-consumer_secret = "CONSUMER_SECRET"
-access_token = "ACCESS_TOKEN"
-access_token_secret = "ACCESS_TOKEN_SECRET"
+API_KEY = keys["API_KEY"]
+API_SECRET_KEY = keys["API_SECRET_KEY"]
+ACCESS_TOKEN = keys["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = keys["ACCESS_TOKEN_SECRET"]
 
-# Authenticate API client
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+openai.api_key = keys["OPENAPI_SECRET_KEY"]
+
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-# OpenAI API key
-openai.api_key = "OPENAPI_SECRET_KEY"
 
-def reply_to_mentions():
-    mentions = api.mentions_timeline()
-    for mention in mentions:
-        if mention.in_reply_to_status_id is None:
-            message = mention.text
-            response = openai.Completion.create(
-                engine="text-davinci-002",
-                prompt=f"{message}\n",
-                max_tokens=100,
-                n=1,
-                stop=None,
-                temperature=0.5,
-            ).choices[0].text
-            api.update_status(
-                status=f"@{mention.user.screen_name} {response}"
-                in_reply_to_status_id=mention.id,
-            )
+def retweet_comment_and_like():
+    """Retweets Twitter posts containing a specific hashtag, likes the tweet and comments on it too"""
+    hashtags = "#saveoursharks OR #sharkawareness OR #sharklover OR #savesharks OR #sharkdiving OR #ilovesharks OR #protectsharks"
+    for tweet in Cursor(
+        api.search_tweets, q=hashtags, lang="en", tweet_mode="extended"
+    ).items(2):
+        # if tweet.lang == "en":
+        try:
+            if not tweet.retweeted:
+                api.retweet(tweet.id)
+                tweet.favorite()
+                status = api.get_status(tweet.id, tweet_mode="extended")
+                screen_name = status.user.screen_name
+                message = f"@{screen_name} Great tweet! I really enjoyed it."
+                api.update_status(message, in_reply_to_status_id=tweet.id)
+                print("Retweeted tweet: " + tweet.full_text)
+            # except tweepy.errors.TweepyException as error:
+        except Exception as error:
+            print("Error: " + str(error))
+
+
+retweet_comment_and_like()
